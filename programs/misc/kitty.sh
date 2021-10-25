1cb9a19466af7a5d1a192f33ad8c0968ba680af4
AdnanValdes <adravaldes+github@gmail.com>
2021-10-24 18:15:28 -0700

    Add kitty theme conf

--- programs/misc/kitty.sh
+++ programs/misc/kitty.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo apt install kitty
+set THEME https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/gruvbox_dark.conf
+wget "$THEME" -P ../.config/kitty/
+mv gruvbox_dark.conf theme.conf
+
