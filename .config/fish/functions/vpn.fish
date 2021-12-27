a84f6aeba734b75533cb9486a6137f9931492a43
Adnan Valdes <adravaldes+github@gmail.com>
2021-12-27 15:57:08 -0800

    Update VPN function to check ifconfig

--- .config/fish/functions/vpn.fish
+++ .config/fish/functions/vpn.fish
@@ -1,6 +1,8 @@
 function vpn
-	if  command sudo surfshark-vpn status | grep "Not connected"
+	if  ! ifconfig | grep -q tun0
+        echo "No VPN found; starting Surfshark..."
 		command sudo surfshark-vpn attack
+        command  curl -ks https://www.dnsleaktest.com | grep hello | sed -e 's/<[^>]*>//g' -e 's/Hello/On DNSLeakTest, your IP is:/g' -e 's/^[ \t]*//;s/[ \t]*$//'
 	else
 		echo "Connection found. Closing VPN"
 		command sudo surfshark-vpn down
